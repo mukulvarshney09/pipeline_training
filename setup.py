9e892d21c1e2e5acc16fa72f1ea0518af12bc5c6
Mukul Varshney <mukulvarshney420@gmail.com>
2023-10-02 08:33:00 +0530

    all file edit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name = "project_name",
+    version = "0.0.1",
+    author = "mukul",
+    author_email = "mukulvarshney420@gmail.com",
+    packages = find_packages(),
+    install_requires = ["pandas", "numpy", "flask"]
+    )
